186869550d84021cdef660c2968a0e8e7b1ba66f
Valtteri Pajunen <valtteri.pajunen@helsinki.fi>
2015-01-21 17:26:50 +0200

    Exercises 1-4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,8 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
+	has_many :ratings
+
+	def average
+		(ratings_per_beer = Rating.where beer_id:self.id).reduce(0) { |sum, rating| sum + rating.score } / ratings_per_beer.count
+	end
 end
