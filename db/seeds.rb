0f3b99a054ce947b02a597ccf7f55f2953ddbe5a
Valtteri Pajunen <valtteri.pajunen@helsinki.fi>
2015-01-25 16:52:34 +0200

    Added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 b1 = Brewery.create name:"Koff", year:1897
 b2 = Brewery.create name:"Malmgard", year:2001
 b3 = Brewery.create name:"Weihenstephaner", year:1042
+b4 = Brewery.create name:"BrewDog", year:2007
 
 b1.beers.create name:"Iso 3", style:"Lager"
 b1.beers.create name:"Karhu", style:"Lager"
@@ -9,3 +10,5 @@ b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
 b2.beers.create name:"X Porter", style:"Porter"
 b3.beers.create name:"Hefezeizen", style:"Weizen"
 b3.beers.create name:"Helles", style:"Lager"
+b4.beers.create name:"Punk IPA", style:"IPA"
+b4.beers.create name:"Nanny State", style:"lowalcohol"
