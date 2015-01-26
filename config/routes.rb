4a57d51d17dfe899bac824e7761fdd53c6dd06e4
Valtteri Pajunen <valtteri.pajunen@helsinki.fi>
2015-01-26 16:43:28 +0200

    Week 3: Exercises 1-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  resources :beers
 
+  resources :users
+  resources :beers
   resources :breweries
+  resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :delete]
 
   root 'breweries#index'
   get 'kaikki_bisset', to: 'beers#index'
-
-  resources :ratings, only: [:index, :new, :create, :destroy]
+  get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   # get 'ratings', to: 'ratings#index'
   # get 'ratings/new', to:'ratings#new'
