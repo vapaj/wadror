0dae203e7a0789d530f5f59e5e6092fe1c821e18
Valtteri Pajunen <valtteri.pajunen@helsinki.fi>
2015-02-12 15:09:24 +0200

    Week5: exercises 1-3

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
+gem 'httparty'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
@@ -37,6 +37,7 @@ group :test do
   gem 'factory_girl_rails'
   gem 'capybara'
   gem 'launchy'
+  gem 'webmock'
 end
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ spec/features/places_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name:"Oljenkorsi", id: 1 ) ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+  it "if multiple is returned, all are shown at the page" do
+  	allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name:"Oljenkorsi", id: 1 ), Place.new( name:"Pari Hiivaa", id: 2 ) ]
+    )
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Pari Hiivaa"
+  end
+  it "if none is returned, show warning text" do
+  	visit places_path
+  	city = 'kumpula'
+  	allow(BeermappingApi).to receive(:places_in).with(city).and_return(
+      [ ]
+    )
+    fill_in('city', with: city)
+    click_button "Search"
+    expect(page).to have_content "No locations in #{city}"
+  end
+end
