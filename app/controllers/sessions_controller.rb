6e137ee1032c06215f80350a3d8bf9948ab070b6
Valtteri Pajunen <valtteri.pajunen@helsinki.fi>
2015-02-19 17:59:08 +0200

    don't create session when suspended user tries to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
   def create
       user = User.find_by username: params[:username]
       if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
         if user.suspended
           redirect_to :back, notice: "Your account has been suspended :-( contact admins for further details"
         else
+          session[:user_id] = user.id
           redirect_to user_path(user), notice: "Welcome back!"
         end
       else
